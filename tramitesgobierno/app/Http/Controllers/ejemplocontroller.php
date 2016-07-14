9ceea7612860956b32f290c8acfef5221f63caf7
Alberto <cuen2504@gmail.com>
2016-07-14 11:12:35 -0600

    modelo y controlador de acta

--- tramitesgobierno/app/Http/Controllers/ejemplocontroller.php
+++ tramitesgobierno/app/Http/Controllers/ejemplocontroller.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests;
+use App\Acta;
 
 class ejemplocontroller extends Controller
 {
@@ -11,6 +12,20 @@ class ejemplocontroller extends Controller
   	$usuario=$Request->input('usuario');
 
   	return view('/principal',compact('usuario'));
+  }
+
+  public function enviarActa(Requests $Requests){
+  	$acta = new Acta();
+
+  	$acta->curp = $Request->input('curp');
+  	$acta->nombre = $Request->input('nombre')
+  	$acta->apellidop = $Request->input('apellidoPaterno');
+  	$acta->apellidom = $Request->input('apellidoMaterno');
+  	$acta->fechanacimiento = $Request->input('dia','-','mes','-', 'año');
+
+  	$acta->save();
+
+  	return redirect('/principal');
   }
 
 }
--- tramitesgobierno/resources/views/tramitarActaNac.blade.php
+++ tramitesgobierno/resources/views/tramitarActaNac.blade.php
@@ -4,34 +4,45 @@
 @stop
 
 @section('contenido')
-<form action="#" method="POST">
+<form action="{{url('enviarActa')}}" method="POST">
 	<input type="hidden" name="_token" value="{{csrf_token()}}">
 	<div class="form-group">
-		<label for="">Nombre:</label>
-		<input type="text" class="form-control" name="nombre">
+		<h4>CURP del solicitante</h4>
+		<label for="">CURP:</label>
+		<input type="text" class="form-control" name="curp" maxlength="18">
+	</div>
+	<hr>
+	<div class="form-group">		
+		<h4>Nombre del solicitante</h4>
+		<label for="">Nombre(s):</label>
+		<input type="text" class="form-control" name="nombre" maxlength="20">
+		<label for="">Apellido paterno:</label>
+		<input type="text" class="form-control" name="apellidoPaterno" maxlength="20">
+		<label for="">Apellido materno:</label>
+		<input type="text" class="form-control" name="apellidoMaterno" maxlength="20">
 	</div>
 	<hr>
 	<div class="form-group">
 		<h4>Fecha de nacimiento</h4>
 		<label for"">Día</label>
-		<input type="text" class="form-control" name="dia" value="">
+		<input type="text" class="form-control" name="dia" value="" maxlength="2">
 		<label for"">Mes</label>
-		<input type="text" class="form-control" name="mes" value="">
+		<input type="text" class="form-control" name="mes" value="" maxlength="2">
 		<label for="">Año</label>
-		<input type="text" class="form-control" name="año" value="">
+		<input type="text" class="form-control" name="año" value="" maxlength="4">
 	</div>
 	<hr>
 	<div class="form-group">
 		<h4>Lugar para recoger el documento</h4>
 		<div class="radio">
   			<label>
-    			<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
+    			<input type="radio" name="radios" id="optionsRadios1" value="option1" checked>
    				 Unidad de Servicios Estatales
   			</label>
 		</div>
 		<div class="radio">
 		 	<label>
-		    	<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
+		    	<input type="radio" name="radios" id="optionsRadios2" value="option2">
 		    	Ayuntamiento
 			</label>
 		</div>
