04f590a377c1719f69818fd2888bf771261317cb
Oscar <millan.rivas.oscar@gmail.com>
2016-07-20 11:53:25 -0600

    vista de notificaciones, vista registro de usuario

--- tramitesgobierno/resources/views/infoperfil.blade.php
+++ tramitesgobierno/resources/views/infoperfil.blade.php
@@ -1,23 +1,25 @@
 @extends('mostrarperfil')
 @section('info') 
 <form action="" method="POST" class="col-xs-8">
-		<label for="">Nombre:</label>
-		<input type="text" class="form-control" name="nombre" maxlength="18" value="">
+		
+		<label for="">Nombre:</label>	<br>
+		<span class="label label-success" value="">nombre</span><br>
+		
 
-			<label for="">Correo:</label>
-		<input type="text" class="form-control" name="correo" maxlength="18" value="" >
+			<label for="">Correo:</label><br>
+		<span class="label label-success" value="">correo</span><br>
 
-			<label for="">Calle:</label>
-		<input type="text" class="form-control" name="calle" maxlength="18" value="">
+			<label for="">Calle:</label><br>
+		<span class="label label-success" value="">calle</span><br>
 
-			<label for="">Numero:</label>
-		<input type="text" class="form-control" name="numero" maxlength="18" value="">
+			<label for="">Numero:</label><br>
+		<span class="label label-success" value="">numero</span><br>
 
-			<label for="">Colonia:</label>
-		<input type="text" class="form-control" name="colonia" maxlength="18" value="">
+			<label for="">Colonia:</label><br>
+		<span class="label label-success" value="">colonia</span><br>
 
-			<label for="">Codigo Postal:</label>
-		<input type="text" class="form-control" name="cp" maxlength="18" value="">
+			<label for="">Codigo Postal:</label><br>
+		<span class="label label-success" value="">codigo postal </span><br>
 		<br>
 	</form>
 @stop
--- tramitesgobierno/resources/views/notificaciones.blade.php
+++ tramitesgobierno/resources/views/notificaciones.blade.php
@@ -1,4 +1,31 @@
 extends('mostrarperfil')
 @section('info') 
 
+<table class="table table-hover">
+					<thead>
+						<tr>
+							<th>Curp</th>
+							<th>Tipo</th>
+							<th>Estado</th>
+							
+						</tr>
+					</thead>
+					<tbody>
+						@foreach($solicitudes as $s)
+							<tr>
+								<td>{{$s->id}}</td>
+								<td>{{$s->nombre}}</td>
+								<td>{{$s->telefono}}</td>
+								<td><a href="{{url('eliminarCliente')}}/{{$c->id}}" class="btn btn-danger btn-xs"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span>Aceptar</a>
+									<a href="{{url('modificarCliente')}}/{{$c->id}}" class="btn btn-primary btn-xs"><span class="glyphicon glyphicon-edit" aria-hidden="true"></span>Rechazar</a>
+								</td>
+							</tr>
+						@endforeach
+					</tbody>
+				</table>
+
+
+
+
+
 @stop
